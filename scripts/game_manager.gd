52f3ec372edb794417757cb9f0dd96db59bfb9a0
nimilgp <nimilgeorgepradeepngp@gmail.com>
2025-08-31 14:01:05 +0530

    added stopwatch

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,10 +3,8 @@ extends Node
 var money = 10 
 @onready var money_label: Label = $"money-label"
 
-func _process(delta: float) -> void:
-	if money == 0:
-		pass
-		# end game
+var time = 0
+@onready var time_label: Label = $"time-label"
 
 func add_money(amount):
 	money += amount
@@ -16,3 +14,9 @@ func reduce_money(amount):
 	money -= amount
 	money_label.text = "$"+ str(money)
 	
+func _process(delta: float) -> void:
+	time += delta
+	time_label.text = "Time:" + str(snappedf(time,0.01))
+	if money == 0:
+		pass
+		# end game
